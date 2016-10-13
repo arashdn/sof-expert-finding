45cc118df7dd3f0ffe8d929ff4e4b48ca7b5ef4b
Arashdn <arash@dargahi.org>
2016-10-13 09:07:45 +0330

    Init commit

---
+++ src/lucenesearch/Program.java
@@ -0,0 +1,18 @@
+package lucenesearch;
+
+/**
+ *
+ * @author arashdn
+ */
+public class Program
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
